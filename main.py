91c82c6dd3bbb92eac534094df073c381f5872bf
dgierek <damian.gierek5@gmail.com>
2024-12-17 05:38:06 +0100

    Added code for testing total energy

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ from visualization import visualize_trajectory
 # generate_simulation_from_trajectory(x, y, 10, save_path, 'test', make_gif=True)
 
 'Testing generation of simulation from generated_trajectory - magnetic field'
-# (x_B, y_B), info_dict = generate_trajectories('magnetic_field', time_step=0.01, max_simul_steps=3000, box_size=10)
+# (x_B, y_B), info_dict = generate_trajectories('magnetic_field', time_step=0.01, max_simul_steps=1000, box_size=10)
 # fig, ax = plt.subplots()
 # ax.scatter(x_B, y_B, s=0.5)
 #
@@ -67,26 +67,62 @@ from visualization import visualize_trajectory
 #     generate_simulation_from_trajectory(x_g, y_g, 10, save_path, 'gravity_test',
 #                                         'gravity_test_0', make_gif=True, info_dict=info_dict)
 
-'Testing the loading of saved trajectory'
-# x_file = r'simulation_frames/gravity_test_0/gravity_test_x_coords.npy'
-# y_file = r'simulation_frames/gravity_test_0/gravity_test_y_coords.npy'
-# visualize_trajectory(x_file, y_file)
-
 'Testing generation of simulation from generated_trajectory - harmonic_oscillator'
-(x_h, y_h), info_dict = generate_trajectories('harmonic_oscillator', time_step=0.01, max_simul_steps=3000,
-                                              box_size=10)
-fig, ax = plt.subplots()
-ax.scatter(x_h, y_h, s=0.5)
+# (x_h, y_h), info_dict = generate_trajectories('harmonic_oscillator', time_step=0.01, max_simul_steps=3000,
+#                                               box_size=10)
+# fig, ax = plt.subplots()
+# ax.scatter(x_h, y_h, s=0.5)
+#
+# ax.set_xlim(0, 10)
+# ax.set_ylim(0, 10)
+# ax.set_aspect('equal')
+# plt.show(block='True')
+#
+# animate_movement(x_h, y_h, 10, interval=10)
+#
+# save_animation = bool(int(input('Save animation (1/0)?')))
+# if save_animation:
+#     save_path = r'C:\Users\Public\Desktop\Python projects\physics_guesser\simulation_frames'
+#     generate_simulation_from_trajectory(x_h, y_h, 10, save_path, 'harmonic_test',
+#                                         'harmonic_test_0', make_gif=True, info_dict=info_dict)
 
-ax.set_xlim(0, 10)
-ax.set_ylim(0, 10)
-ax.set_aspect('equal')
-plt.show(block='True')
 
-animate_movement(x_h, y_h, 10, interval=10)
+'Testing generation of simulation from generated trajectory - no force:'
+# (x_f, y_f), info_dict = generate_trajectories('no_force', time_step=0.01, max_simul_steps=1000,
+#                                               box_size=10)
+# fig, ax = plt.subplots()
+# ax.scatter(x_f, y_f, s=0.5)
+#
+# ax.set_xlim(0, 10)
+# ax.set_ylim(0, 10)
+# ax.set_aspect('equal')
+# plt.show(block='True')
+#
+# animate_movement(x_f, y_f, 10, interval=10)
+#
+# save_animation = bool(int(input('Save animation (1/0)?')))
+# if save_animation:
+#     save_path = r'C:\Users\Public\Desktop\Python projects\physics_guesser\simulation_frames'
+#     generate_simulation_from_trajectory(x_f, y_f, 10, save_path, 'no_force_test',
+#                                         'no_force_test_0', make_gif=True, info_dict=info_dict)
 
-save_animation = bool(int(input('Save animation (1/0)?')))
-if save_animation:
-    save_path = r'C:\Users\Public\Desktop\Python projects\physics_guesser\simulation_frames'
-    generate_simulation_from_trajectory(x_h, y_h, 10, save_path, 'harmonic_test',
-                                        'harmonic_test_0', make_gif=True, info_dict=info_dict)
+'Testing the loading of saved trajectory for no force'
+# x_file = r'simulation_frames/no_force_test_0/no_force_test_x_coords.npy'
+# y_file = r'simulation_frames/no_force_test_0/no_force_test_y_coords.npy'
+# visualize_trajectory(x_file, y_file, r'simulation_frames/no_force_test_0/info_dict.txt')
+
+'Testing the loading of saved trajectory for magnetic_field'
+# x_file = r'simulation_frames/magnetic_field_test_0/magnetic_field_test_x_coords.npy'
+# y_file = r'simulation_frames/magnetic_field_test_0/magnetic_field_test_y_coords.npy'
+# visualize_trajectory(x_file, y_file, r'simulation_frames/magnetic_field_test_0/info_dict.txt')
+
+'Testing the loading of saved trajectory for 2D harmonic oscillator'
+# x_file = r'simulation_frames/harmonic_test_0/harmonic_test_x_coords.npy'
+# y_file = r'simulation_frames/harmonic_test_0/harmonic_test_y_coords.npy'
+# visualize_trajectory(x_file, y_file, r'simulation_frames/harmonic_test_0/info_dict.txt')
+
+'Testing the loading of saved trajectory for gravity'
+x_file = r'simulation_frames/gravity_test_0/gravity_test_x_coords.npy'
+y_file = r'simulation_frames/gravity_test_0/gravity_test_y_coords.npy'
+visualize_trajectory(x_file, y_file, r'simulation_frames/gravity_test_0/info_dict')
+
