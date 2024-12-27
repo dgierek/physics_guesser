b777cb80356a2e80b6c9525738a805bb27224308
dgierek <damian.gierek5@gmail.com>
2024-12-27 12:56:44 +0100

    Data for testing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 from rocket_simulation import generate_trajectories_analytically, generate_simulation_from_trajectory
 from animation import animate_movement, test_circle
 from visualization import visualize_trajectory_analytically
+import numpy as np
+
 
 'Testing the creation of animation frames from coordinates'
 # x, y = test_circle(5, 5, 3, 30)
@@ -11,7 +13,8 @@ from visualization import visualize_trajectory_analytically
 # generate_simulation_from_trajectory(x, y, 10, save_path, 'test', make_gif=True)
 
 'Testing generation of simulation from generated_trajectory - magnetic field'
-# (x_B, y_B), info_dict = generate_trajectories('magnetic_field', time_step=0.01, max_simul_steps=3000, box_size=10)
+# (x_B, y_B), info_dict = generate_trajectories_analytically('magnetic_field', time_step=0.01,
+#                                                            max_simul_steps=3000, box_size=10)
 # fig, ax = plt.subplots()
 # ax.scatter(x_B, y_B, s=0.5)
 #
@@ -28,39 +31,47 @@ from visualization import visualize_trajectory_analytically
 #     generate_simulation_from_trajectory(x_B, y_B, 10, save_path, 'magnetic_field_test',
 #                                         'magnetic_field_test_0', make_gif=True, info_dict=info_dict
 #                                         )
-#
-# x_file = r'simulation_frames/magnetic_field_test_0/magnetic_field_test_x_coords.npy'
-# y_file = r'simulation_frames/magnetic_field_test_0/magnetic_field_test_y_coords.npy'
-# visualize_trajectory(x_file, y_file, r'simulation_frames/magnetic_field_test_0/info_dict.txt')
+#     x_file = r'simulation_frames/magnetic_field_test_0/magnetic_field_test_x_coords.npy'
+#     y_file = r'simulation_frames/magnetic_field_test_0/magnetic_field_test_y_coords.npy'
+#     visualize_trajectory_analytically(x_file, y_file,
+#                                       r'simulation_frames/magnetic_field_test_0/info_dict.txt')
 
 'Testing generation of simulation from generated_trajectory - gravity'
-(x_g, y_g), info_dict = generate_trajectories_analytically('gravity', time_step=0.01, max_simul_steps=3000, box_size=10)
-fig, ax = plt.subplots()
-ax.scatter(x_g, y_g, s=0.5)
-
-ax.set_xlim(0, 10)
-ax.set_ylim(0, 10)
-ax.set_aspect('equal')
-plt.show(block='True')
-
-animate_movement(x_g, y_g, 10, interval=10)
-
-save_animation = bool(int(input('Save animation (1/0)? ')))
-if save_animation:
-    save_path = r'C:\Users\Public\Desktop\Python projects\physics_guesser\simulation_frames'
-    generate_simulation_from_trajectory(x_g, y_g, 10, save_path, 'gravity_test',
-                                        'gravity_test_0', make_gif=True, info_dict=info_dict)
-
-x_file = r'simulation_frames/gravity_test_0/gravity_test_x_coords.npy'
-y_file = r'simulation_frames/gravity_test_0/gravity_test_y_coords.npy'
-visualize_trajectory_analytically(x_file, y_file, r'simulation_frames/gravity_test_0/info_dict.txt')
+# (x_g, y_g), info_dict = generate_trajectories_analytically('gravity', time_step=0.01, max_simul_steps=3000, box_size=10)
+# fig, ax = plt.subplots()
+# ax.scatter(x_g, y_g, s=0.5)
+#
+# ax.set_xlim(0, 10)
+# ax.set_ylim(0, 10)
+# ax.set_aspect('equal')
+# plt.show(block='True')
+#
+# animate_movement(x_g, y_g, 10, interval=10)
+#
+# save_animation = bool(int(input('Save animation (1/0)? ')))
+# if save_animation:
+#     save_path = r'C:\Users\Public\Desktop\Python projects\physics_guesser\simulation_frames'
+#     generate_simulation_from_trajectory(x_g, y_g, 10, save_path, 'gravity_test',
+#                                         'gravity_test_0', make_gif=True, info_dict=info_dict)
+#
+# x_file = r'simulation_frames/gravity_test_0/gravity_test_x_coords.npy'
+# y_file = r'simulation_frames/gravity_test_0/gravity_test_y_coords.npy'
+# visualize_trajectory_analytically(x_file, y_file, r'simulation_frames/gravity_test_0/info_dict.txt')
 
 
 'Testing generation of simulation from generated_trajectory - harmonic_oscillator'
-# (x_h, y_h), info_dict = generate_trajectories('harmonic_oscillator', time_step=0.01, max_simul_steps=3000,
-#                                               box_size=10)
+# (x_h, y_h), info_dict = generate_trajectories_analytically('harmonic_oscillator', time_step=0.01,
+#                                                            max_simul_steps=6000, box_size=10)
 # fig, ax = plt.subplots()
 # ax.scatter(x_h, y_h, s=0.5)
+#
+# # getting information from info_dict:
+# r_x, r_y = np.fromstring(info_dict["equilibrium_point"].strip("[]"), sep=" ")
+# v_0_x, v_0_y = np.fromstring(info_dict['initial_velocity'].strip("[]"), sep=" ")
+# ax.scatter(r_x, r_y, c='black', label='Equilibrium point')
+# ax.legend()
+#
+# print(v_0_x, v_0_y)
 #
 # ax.set_xlim(0, 10)
 # ax.set_ylim(0, 10)
@@ -69,37 +80,38 @@ visualize_trajectory_analytically(x_file, y_file, r'simulation_frames/gravity_te
 #
 # animate_movement(x_h, y_h, 10, interval=10)
 #
-# save_animation = bool(int(input('Save animation (1/0)?')))
+# save_animation = bool(int(input('Save animation (1/0)? ')))
 # if save_animation:
 #     save_path = r'C:\Users\Public\Desktop\Python projects\physics_guesser\simulation_frames'
 #     generate_simulation_from_trajectory(x_h, y_h, 10, save_path, 'harmonic_test',
 #                                         'harmonic_test_0', make_gif=True, info_dict=info_dict)
-#
-# x_file = r'simulation_frames/harmonic_test_0/harmonic_test_x_coords.npy'
-# y_file = r'simulation_frames/harmonic_test_0/harmonic_test_y_coords.npy'
-# visualize_trajectory(x_file, y_file, r'simulation_frames/harmonic_test_0/info_dict.txt')
+#     x_file = r'simulation_frames/harmonic_test_0/harmonic_test_x_coords.npy'
+#     y_file = r'simulation_frames/harmonic_test_0/harmonic_test_y_coords.npy'
+#     visualize_trajectory_analytically(x_file, y_file, r'simulation_frames/harmonic_test_0/info_dict.txt')
 
 
 'Testing generation of simulation from generated trajectory - no force:'
-# (x_f, y_f), info_dict = generate_trajectories('no_force', time_step=0.01, max_simul_steps=3000,
-#                                               box_size=10)
-# fig, ax = plt.subplots()
-# ax.scatter(x_f, y_f, s=0.5)
-#
-# ax.set_xlim(0, 10)
-# ax.set_ylim(0, 10)
-# ax.set_aspect('equal')
-# plt.show(block='True')
-#
-# #animate_movement(x_f, y_f, 10, interval=10)
-#
-# save_animation = bool(int(input('Save animation (1/0)? ')))
-# if save_animation:
-#     save_path = r'C:\Users\Public\Desktop\Python projects\physics_guesser\simulation_frames'
-#     generate_simulation_from_trajectory(x_f, y_f, 10, save_path, 'no_force_test',
-#                                         'no_force_test_0', make_gif=True, info_dict=info_dict)
-#     x_file = r'simulation_frames/no_force_test_0/no_force_test_x_coords.npy'
-#     y_file = r'simulation_frames/no_force_test_0/no_force_test_y_coords.npy'
-#     visualize_trajectory(x_file, y_file, r'simulation_frames/no_force_test_0/info_dict.txt')
+(x_f, y_f), info_dict = generate_trajectories_analytically('no_force', time_step=0.01, max_simul_steps=5000,
+                                                           box_size=10)
+fig, ax = plt.subplots()
+ax.scatter(x_f, y_f, s=0.5)
+
+print(x_f[-2:], y_f[-2:])
+
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+ax.set_aspect('equal')
+plt.show(block='True')
+
+animate_movement(x_f, y_f, 10, interval=1)
+
+save_animation = bool(int(input('Save animation (1/0)? ')))
+if save_animation:
+    save_path = r'C:\Users\Public\Desktop\Python projects\physics_guesser\simulation_frames'
+    generate_simulation_from_trajectory(x_f, y_f, 10, save_path, 'no_force_test',
+                                        'no_force_test_0', make_gif=True, info_dict=info_dict)
+    x_file = r'simulation_frames/no_force_test_0/no_force_test_x_coords.npy'
+    y_file = r'simulation_frames/no_force_test_0/no_force_test_y_coords.npy'
+    visualize_trajectory_analytically(x_file, y_file, r'simulation_frames/no_force_test_0/info_dict.txt')
 
 
