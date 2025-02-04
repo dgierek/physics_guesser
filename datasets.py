31786cc354e0b116aa3a2cc33d33ae8c70d1e053
dgierek <damian.gierek5@gmail.com>
2025-02-04 19:14:43 +0100

    Getting the right batching

--- datasets.py
+++ datasets.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, Sampler
 from torchvision import transforms
 from visualization import visualise_batch
 import re
@@ -39,7 +39,7 @@ class ImageDataset(Dataset):
             raise IndexError("Index out of range")
 
         # Load three subsequent images
-        img_paths = self.image_files[idx:idx+3]
+        img_paths = self.image_files[idx:idx + 3]
         images = []
 
         for img_path in img_paths:
@@ -50,22 +50,37 @@ class ImageDataset(Dataset):
 
         # Stack images into a single tensor
         images_tensor = torch.stack(images)
-        return images_tensor
+        return list(range(idx, idx + 3)), images_tensor
 
 
-'Usage example:'
+def collate_fn(batch):
+    batch_indices = []
+    batch_images = []
+    for idx, images in batch:
+        batch_indices.extend(idx)
+        batch_images.append(images)
+    batch_images = torch.stack(batch_images)
+    return torch.tensor(batch_indices), batch_images
+
+# 'Usage example:'
 # transform = transforms.Compose([
 #     transforms.Resize((64, 64)),  # down sampling the resolution of the images
 #     transforms.ToTensor()
 # ])
 # dataset = ImageDataset(directory=r'simulation_frames/gravity_test_0/simulation_snapshots', transform=transform)
-# dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+# dataloader_batch_1 = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=collate_fn)
+# dataloader_batch_4 = DataLoader(dataset, batch_size=4, shuffle=False, collate_fn=collate_fn)
 #
 # print('Length of dataset:', dataset.__len__())
-# print('len(dataset)', len(dataset))
-# single_batch = next(iter(dataloader))
-# print('Batch shape:', single_batch.shape)  # should print torch.Size([1, 3, 3, 64, 64])
+# print('Length of single batch dataloader:', len(dataloader_batch_1))
+# print('Length of four batch dataloader:', len(dataloader_batch_4)) # prints 8. Probably makes sense as 32/4=8
+# single_batch = next(iter(dataloader_batch_1))
+# four_batch = next(iter(dataloader_batch_4))
+# print('Single batch shape:', single_batch[1].shape)
+# print('Four batch shape:', four_batch[1].shape)
+# print('Single batch indexes:', single_batch[0])
+# print('Four batch indexes:', four_batch[0])
 #
-# for batch in dataset:
-#     print(batch.shape)
+# for batch in four_batch[1]:
+#     visualise_batch(batch)
 
